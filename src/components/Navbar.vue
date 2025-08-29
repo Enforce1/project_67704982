d6dd61481538323350ebf740f0edc6e454baf2cd
unknown <breezzaza28@gmail.com>
2025-08-28 22:34:07 -0700

    update date 8/28/2025

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,8 @@
-<template>
-  <nav class="navbar navbar-expand-lg"  style="background-color:#FFAD47;" data-bs-theme="dark">
-  <div class="container">
+ <template lang="">
+    <div>
+        
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -8,24 +10,24 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">Home</a>
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/showproduct">Show Product</a>
+          <a class="nav-link" href="#">Link</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            login
+            Dropdown
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">login</a></li>
+            <li><a class="dropdown-item" href="#">Action</a></li>
             <li><a class="dropdown-item" href="#">Another action</a></li>
             <li><hr class="dropdown-divider"></li>
             <li><a class="dropdown-item" href="#">Something else here</a></li>
           </ul>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/about">About</a>
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
         </li>
       </ul>
       <form class="d-flex" role="search">
@@ -34,8 +36,14 @@
       </form>
     </div>
   </div>
-</nav> 
+</nav>
 
-  <router-view/>
+    </div>
+ </template>
 
-</template>
+ <script>
+ export default {
+    
+ }
+ </script>
+ 
