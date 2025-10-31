11091f85338b86746d9756bc5bfe1d5734dfce2f
unknown <breezzaza28@gmail.com>
2025-10-30 21:52:48 -0700

    week 10

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,13 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">Home</a>
-        </li>
+
+        <!-- แสดงเฉพาะเมื่อเข้าสู่ระบบแล้ว -->
+        <template v-if="isLoggedIn"> 
+
+        
+      
+
         <li class="nav-item">
           <a class="nav-link" href="/showproduct">Show Product</a>
         </li>
@@ -37,26 +41,35 @@
             <li><a class="dropdown-item" href="/customer_edit">Customer edit</a></li>
           </ul>
         </li>
-        
 
+        <li class="nav-item">
+          <a class="nav-link" href="#" @click="logout">logout</a>
+        </li>
+        </template>
+        
+<template v-else>
+  <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             login
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">login</a></li>
-            <li><a class="dropdown-item" href="#"></a>logout</li>
+            <li><a class="dropdown-item" href="/login">login</a></li>
+            <li><a class="dropdown-item" href="#" @click="logout">logout</a></li>
             <li><hr class="dropdown-divider"></li>
             <li><a class="dropdown-item" href="add_customer">Register</a></li>
           </ul>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="/about">About</a>
-        </li>
+        </li> 
+        </template>
       </ul>
       <form class="d-flex" role="search">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button class="btn btn-outline-success" type="submit">Search</button>
+        <button class="btn btn-outline-success  " type="submit">Search</button>
       </form>
     </div>
   </div>
@@ -65,3 +78,55 @@
   <router-view/>
 
 </template>
+
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {
+      isLoggedIn: false,
+    };
+  },
+  mounted() {
+    // ตรวจสอบสถานะเมื่อโหลดหน้า
+    this.checkLogin();
+  },
+  methods: {
+    checkLogin() {
+      this.isLoggedIn = localStorage.getItem("customerLogin") === "true";
+    },
+    logout() {
+      if (confirm("ต้องการออกจากระบบหรือไม่?")) {
+        // เคลียร์ข้อมูลทั้งหมดที่เกี่ยวข้องกับการล็อกอิน
+        localStorage.removeItem("customerLogin");
+        localStorage.removeItem("username");
+        localStorage.removeItem("token");
+        this.isLoggedIn = false;
+
+        // กลับไปหน้าเมนูหลัก
+        this.$router.push("/");
+      }
+    },
+  },
+  watch: {
+    // เมื่อเปลี่ยนเส้นทาง ให้ตรวจสอบสถานะการล็อกอินใหม่
+    $route() {
+      this.checkLogin();
+    },
+  },
+};
+</script>
+
+
+<style scoped>
+.navbar {
+  background-color: #86bfe7ff !important;
+}
+.nav-link {
+  color: white !important;
+  font-weight: 500;
+}
+.nav-link:hover {
+  text-decoration: underline;
+}
+</style>
